08f04ded37cb6a6987cbb850fdb7d1c29ab97e2b
Konstantine1307 <john.farah@icloud.com>
2022-12-21 17:40:20 +1300

    Added some styling

--- components/intro.js
+++ components/intro.js
@@ -1,9 +1,18 @@
 export default function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-4xl md:text-5xl font-bold tracking-tighter leading-tight md:pr-8">
-        Johnny & Leslie Travel Blog.
-      </h1>
+    <section>    
+      <div className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+
+          <h1 className="text-4xl md:text-5xl font-bold tracking-tighter leading-tight md:pr-8">
+            Johnny & Leslie Travel Blog.
+          </h1>
+      </div>
+  
+      <div>
+        <h3 className="text-xl font-normal  md: pr-8">Follow our adventures, places and people we have visited and met in our trips.</h3>
+      
+      <p className="my-8 font-bold italic text-right">OUR LATEST POST:</p>
+      </div>
     </section>
   );
 }
